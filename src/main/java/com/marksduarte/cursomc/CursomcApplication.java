ca1455d616c815e059312555d8db660e19ebd3f0
Marks Duarte <marksduarte@gmail.com>
2019-02-28 09:12:03 -0300

    Criando operação de instanciação

--- src/main/java/com/marksduarte/cursomc/CursomcApplication.java
+++ src/main/java/com/marksduarte/cursomc/CursomcApplication.java
@@ -1,13 +1,34 @@
 package com.marksduarte.cursomc;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.marksduarte.cursomc.domain.Categoria;
+import com.marksduarte.cursomc.repositories.CategoriaRepository;
+
+
 @SpringBootApplication
-public class CursomcApplication {
+public class CursomcApplication implements CommandLineRunner  {
+	
+	@Autowired
+	private CategoriaRepository categoriaRepo;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+		// O atributo id é passado como nulo, pois o campo no banco é autoincrement
+		Categoria cat1 = new Categoria(null, "Informática");
+		Categoria cat2 = new Categoria(null, "Escritório");
+		
+		categoriaRepo.save(cat1);
+		categoriaRepo.save(cat2);
+		
 	}
 
 }

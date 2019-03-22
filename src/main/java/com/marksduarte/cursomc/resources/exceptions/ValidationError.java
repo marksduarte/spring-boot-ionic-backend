b8155d253e9038bf4c717950a1c0082e4e3296a6
Marks Duarte <marksduarte@gmail.com>
2019-03-22 13:50:01 -0300

    Personalizando o erro de validação.

---
+++ src/main/java/com/marksduarte/cursomc/resources/exceptions/ValidationError.java
@@ -0,0 +1,28 @@
+package com.marksduarte.cursomc.resources.exceptions;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ValidationError extends StandardError {
+    private static final long serialVersionUID = 1L;
+
+    private List<FieldMessage> errors = new ArrayList<>();
+
+    public ValidationError(Integer status, String msg, Long timeStamp) {
+        super(status, msg, timeStamp);
+    }
+
+    /**
+     * Para que a lista de errors no response Json Errors,
+     * o método foi renomeado para getErrors.
+     *
+     * @return
+     */
+    public List<FieldMessage> getErrors() {
+        return errors;
+    }
+
+    public void addError(String fieldName, String message) {
+        this.errors.add(new FieldMessage(fieldName, message));
+    }
+}

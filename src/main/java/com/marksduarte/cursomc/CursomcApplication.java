0cb1aecff1308f3f8d142c0c70b59abd2e9675d2
Marks Duarte <marksduarte@gmail.com>
2019-05-02 21:14:38 -0300

    Configuracao e envio do projeto para producao

--- src/main/java/com/marksduarte/cursomc/CursomcApplication.java
+++ src/main/java/com/marksduarte/cursomc/CursomcApplication.java
@@ -1,35 +1,8 @@
 package com.marksduarte.cursomc;
 
-import java.text.SimpleDateFormat;
-import java.util.Arrays;
-
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import com.marksduarte.cursomc.domain.Categoria;
-import com.marksduarte.cursomc.domain.Cidade;
-import com.marksduarte.cursomc.domain.Cliente;
-import com.marksduarte.cursomc.domain.Endereco;
-import com.marksduarte.cursomc.domain.Estado;
-import com.marksduarte.cursomc.domain.ItemPedido;
-import com.marksduarte.cursomc.domain.Pagamento;
-import com.marksduarte.cursomc.domain.PagamentoComBoleto;
-import com.marksduarte.cursomc.domain.PagamentoComCartao;
-import com.marksduarte.cursomc.domain.Pedido;
-import com.marksduarte.cursomc.domain.Produto;
-import com.marksduarte.cursomc.domain.enums.EstadoPagamento;
-import com.marksduarte.cursomc.domain.enums.TipoCliente;
-import com.marksduarte.cursomc.repositories.CategoriaRepository;
-import com.marksduarte.cursomc.repositories.CidadeRepository;
-import com.marksduarte.cursomc.repositories.ClienteRepository;
-import com.marksduarte.cursomc.repositories.EnderecoRepository;
-import com.marksduarte.cursomc.repositories.EstadoRepository;
-import com.marksduarte.cursomc.repositories.ItemPedidoRepository;
-import com.marksduarte.cursomc.repositories.PagamentoRepository;
-import com.marksduarte.cursomc.repositories.PedidoRepository;
-import com.marksduarte.cursomc.repositories.ProdutoRepository;
 
 
 @SpringBootApplication

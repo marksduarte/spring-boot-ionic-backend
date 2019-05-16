f034703fbf68a5f2512ecd31753db91a14966ce4
Marks Duarte <marksduarte@gmail.com>
2019-05-16 11:18:58 -0300

    Configuracao inicial do Spring Security

---
+++ src/main/java/com/marksduarte/cursomc/config/SecurityConfig.java
@@ -0,0 +1,68 @@
+package com.marksduarte.cursomc.config;
+
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+	@Autowired
+	private Environment environment;
+
+	private static final String[] PUBLIC_MATCHERS = { "/h2-console/**" };
+
+	private static final String[] PUBLIC_MATCHERS_GET = { "/categorias/**", "/produtos/**" };
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+
+		/**
+		 * Verifica os profiles ativos e habilita o acesso ao h2-console.
+		 */
+		if (Arrays.asList(environment.getActiveProfiles()).contains("test")) {
+			http.headers().frameOptions().disable();
+		}
+
+		/**
+		 * Desabilita a proteção de ataque em sistema stateless.
+		 */
+		http.cors().and().csrf().disable();
+
+		http.authorizeRequests()
+			.antMatchers(HttpMethod.GET, PUBLIC_MATCHERS_GET).permitAll()
+			.antMatchers(PUBLIC_MATCHERS).permitAll()
+			.anyRequest()				
+			.authenticated();
+		/**
+		 * Desabilita a criação de sessão de usuário pelo backend
+		 */
+		http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+	}
+
+	/**
+	 * Permite o acesso aos endpoints por multiplas fontes (cross-origin),
+	 * utilizando as configurações básicas. Por padrão esse tipo de acesso não é
+	 * permitido.
+	 * 
+	 * @return
+	 */
+	@Bean
+	CorsConfigurationSource corsConfigurationSource() {
+		final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		source.registerCorsConfiguration("/**", new CorsConfiguration().applyPermitDefaultValues());
+		return source;
+	}
+}
